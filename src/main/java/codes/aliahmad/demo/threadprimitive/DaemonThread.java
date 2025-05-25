7a85fdab164717f27c3485cf751f6666ba15f122
Ali Ahmad <aliahmadcse@gmail.com>
2025-05-26 01:54:30 +0500

    daemon threads

--- src/main/java/codes/aliahmad/demo/threadprimitive/DaemonThread.java
+++ src/main/java/codes/aliahmad/demo/threadprimitive/DaemonThread.java
@@ -1,31 +1,21 @@
 package codes.aliahmad.demo.threadprimitive;
 
-import codes.aliahmad.doc.primeutils.PrimeUtils;
 
-import java.util.Scanner;
+public class DaemonThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread daemon = new Thread(() -> {
+            while (true) {
+                System.out.println("Daemon still working...");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        });
+        daemon.setDaemon(true); // Set as daemon
+        daemon.start();
 
-public class DaemonThread
-{
-  public static void main(String[] args)
-  {
-    while (true)
-    {
-      Scanner sc = new Scanner(System.in);
-
-      System.out.print("\n I can tell you the nth prime number. Enter n: ");
-      int n = sc.nextInt();
-
-      if (n == 0)
-      {
-        break;
-      }
-
-      Runnable r = () -> System.out.println(STR. "\n Value of \{ n }th prime number is \{ PrimeUtils.calculateNthPrime(n) }\n" );
-
-      Thread thread = new Thread(r);
-      thread.setDaemon(true);
-      thread.start();
-
+        System.out.println("Main thread done.");
+        // JVM exits immediately after this
     }
-  }
 }
